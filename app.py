52f4d58e75618834803d9fd5f0b7556deaab954c
ewerttrewe <93892998+ewerttrewe@users.noreply.github.com>
2024-03-29 15:47:36 +0100

    chore: db connection change, try & except block
    changed the posted data to db
    changed the way connection is established
    try and except block for catching errors during connection to db or inserting data

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
+#standard imports
+from dotenv import load_dotenv
+import os
+#3rd party imports
 from flask import Flask
 from flask_restful import Api
+from flask_jwt_extended import JWTManager
+#app imports
 from .users import UsersRegistrationView
-from dotenv import load_dotenv
+
 
 
 load_dotenv()
+
 app = Flask(__name__)
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+jwt = JWTManager(app)
+
 api = Api(app)
 
 
--- users.py
+++ users.py
@@ -1,11 +1,15 @@
-from flask_restful import Resource
-from flask import request
-
+#app imports
 from .utils import is_correct_url
-
+#standard imports
 from dotenv import load_dotenv
 import mysql.connector
 import os
+#3rd party imports
+from flask_restful import Resource
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+from flask import request
+from flask import jsonify
+
 
 load_dotenv()
 
@@ -15,30 +19,33 @@ class UsersRegistrationView(Resource):
         # asigining data
         try:
             data = request.get_json()
+            print(data)
             if is_correct_url(data["registered_site"]):
                 email = data["email"]
                 registered_site = data["registered_site"]
-                password = data["password"]
+                
+                #connecting to db
                 cnx = mysql.connector.connect(
                     user=os.getenv("USER"),
                     password=os.getenv("PASSWORD"),
                     host=os.getenv("HOST"),
                     database=os.getenv("DATABASE"),
                 )
+                print(cnx.is_connected())
+                #opening cursor & executing query & commit to db
                 cursor = cnx.cursor()
                 cursor.execute(
-                    "INSERT INTO users" "(email, site, password)" "VALUES(%s, %s, %s)",
+                    "INSERT INTO users" "(email, registered_site)" "VALUES(%s, %s)",
                     (
                         email,
                         registered_site,
-                        password,
                     ),
                 )
                 cnx.commit()
                 cursor.close()
                 cnx.close()
-                return "data posted to db"
+                return "data posted to db" 
             else:
                 return "please provide correct url address"
-        except:
-            return "your address email must be unique!"
+        except Exception as e:
+            return jsonify({"something went wrong!":str(e)})
